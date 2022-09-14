cb17519186c0cb75ea065e5a7cf9eb4d7aacdfe2
GMN630 <108471217+GMN630@users.noreply.github.com>
2022-09-14 13:03:30 +0530

    Update player.py

--- plugins/player.py
+++ plugins/player.py
@@ -176,7 +176,7 @@ async def play(_, message: Message):
 
         file_name = get_file_name(audio)
         title = file_name
-        thumb_name = "https://te.legra.ph/file/700b6db739b57216a771d.jpg"
+        thumb_name = "https://te.legra.ph/file/1a600ff2a12b3e15cc708.jpg"
         thumbnail = thumb_name
         duration = round(audio.duration / 60)
         views = "Locally added"
@@ -261,7 +261,7 @@ async def play(_, message: Message):
 
         except Exception as e:
             title = "NaN"
-            thumb_name = "https://te.legra.ph/file/700b6db739b57216a771d.jpg"
+            thumb_name = "https://te.legra.ph/file/1a600ff2a12b3e15cc708.jpg"
             duration = "NaN"
             views = "NaN"
             keyboard = InlineKeyboardMarkup(

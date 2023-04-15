6145c048d41b2b2cb23cfa41ebadae964eb1bdde
LoveHeart <113758022+LeeyooMuzic@users.noreply.github.com>
2023-04-16 02:26:45 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -4,18 +4,18 @@ import aiohttp
 from os import getenv
 from dotenv import load_dotenv
 
-# 𝗜𝗻𝘁𝗲𝗿𝗻𝗮𝗹 𝗩𝗮𝗿𝗶𝗮𝗯𝗹𝗲𝘀 (@𝗚𝗿𝗼𝗼𝘁_𝗡𝗲𝘁𝘄𝗼𝗿𝗸 @𝗠𝘆𝗡𝗮𝗺𝗲𝗜𝘀𝗚𝗿𝗼𝗼𝘁)
+# 𝗜𝗻𝘁𝗲𝗿𝗻𝗮𝗹 𝗩𝗮𝗿𝗶𝗮𝗯𝗹𝗲𝘀 (@𝗚𝗿𝗼𝗼𝘁_𝗡𝗲𝘁𝘄𝗼𝗿𝗸 @𝗥𝗝𝗯𝗿0)
 load_dotenv()
 que = {}
 admins = {}
 aiohttpsession = aiohttp.ClientSession()
 
-# 𝗥𝗲𝗾𝘂𝗶𝗿𝗲𝗱 𝗩𝗮𝗿𝗶𝗮𝗯𝗹𝗲𝘀 //🌱𝗢𝘄𝗻𝗲𝗿 @𝗚𝗿𝗼𝗼𝘁_𝗡𝗲𝘁𝘄𝗼𝗿𝗸 @𝗠𝘆𝗡𝗮𝗺𝗲𝗜𝘀𝗚𝗿𝗼𝗼𝘁
+# 𝗥𝗲𝗾𝘂𝗶𝗿𝗲𝗱 𝗩𝗮𝗿𝗶𝗮𝗯𝗹𝗲𝘀 //🌱𝗢𝘄𝗻𝗲𝗿 @𝗚𝗿𝗼𝗼𝘁_𝗡𝗲𝘁𝘄𝗼𝗿𝗸 @𝗥𝗝𝗯𝗿0
 API_HASH = getenv("API_HASH", "XXXXX")
 API_ID = int(getenv("API_ID", "XXXXX"))
 ASSISTANT_NAME = getenv("ASSISTANT_NAME", "XXXXX")
 ASSISTANT_USERNAME = getenv("ASSISTANT_USERNAME", "XXXXX")
-BOT_IMAGE = getenv("BOT_IMAGE", "https://telegra.ph/file/b9046390e87cbc3c5b6f0.jpg")
+BOT_IMAGE = getenv("BOT_IMAGE", "https://telegra.ph//file/6fbc6c7bfa7af3204f2c1.jpg")
 BOT_NAME = getenv("BOT_NAME", "XXXXX")
 BOT_TOKEN = getenv("BOT_TOKEN", "12345:XXXXX")
 BOT_USERNAME = getenv("BOT_USERNAME", "XXXXX")
@@ -25,7 +25,7 @@ OWNER_NAME = getenv("OWNER_NAME", "𝗜𝗮𝗺 𝗚𝗿𝗼𝗼𝘁")
 OWNER_USERNAME = getenv("OWNER_USERNAME", "Groot_Network")
 SOURCE_CODE = getenv("SOURCE_CODE", "https://t.me/rjbr0")
 STRING_SESSION = getenv("STRING_SESSION", "session")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5342963115").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5056205033").split()))
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/groot_network")
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "https://t.me/rjbr0")
 
